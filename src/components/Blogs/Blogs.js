1f5780feac800dd1651c7628dcb94b568c5bd07d
mjaumi <mjaumi2864@gmail.com>
2022-05-31 01:14:45 +0600

    blogs component done

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -4,9 +4,13 @@ const Blogs = ({ currentSection }) => {
 
     // rendering blogs component here
     return (
-        <section className='relative border-t-4 border-accent/50 section'>
+        <section className='relative border-t-4 border-accent/50 section fp-responsive md:h-screen'>
             <div className='sticky z-30 bg-primary w-full h-full overflow-hidden'>
-                <h1 className={`absolute text-accent opacity-20 text-4xl md:text-8xl font-black -rotate-90 left-0 -translate-x-1/3 md:translate-x-0 duration-500 ${currentSection === 'Blogs' ? 'top-[42%] md:top-[45%] -translate-y-1/2' : 'top-[150%]'}`}>My Blogs</h1>
+                <h1 className={`hidden md:block absolute text-accent opacity-20 text-4xl md:text-8xl font-black -rotate-90 left-0 -translate-x-1/3 md:translate-x-0 duration-500 ${currentSection === 'Blogs' ? 'top-[42%] md:top-[45%] -translate-y-1/2' : 'top-[150%]'}`}>My Blogs</h1>
+                <h2 className='mt-10 text-accent font-bold text-3xl block md:hidden'>My Blogs</h2>
+                <div className={`w-[90%] md:w-[75%] mt-10 mb-20 md:mb-0 md:mt-0 mx-auto h-full flex flex-col md:flex-row items-center justify-center ${currentSection === 'Blogs' ? 'md:animate-bottom-top-fade-in' : 'md:opacity-0'}`}>
+                    <h3 className='text-5xl font-bold text-accent opacity-20'>Coming Soon. Stay Tuned...</h3>
+                </div>
             </div>
         </section>
     );
--- src/components/Expertise/Expertise.js
+++ src/components/Expertise/Expertise.js
@@ -6,8 +6,8 @@ const Expertise = ({ currentSection }) => {
     return (
         <section className='relative border-t-4 border-accent/50 section fp-responsive md:h-screen'>
             <div className='sticky z-30 bg-primary w-full h-full overflow-hidden'>
-                <h1 className={`hidden md:block absolute text-accent opacity-20 text-4xl md:text-8xl font-black -rotate-90 left-0 -translate-x-1/3 md:translate-x-0 duration-500 ${currentSection === 'Expertise' ? 'top-[42%] md:top-[45%] -translate-y-1/2' : 'top-[150%]'}`}>Expertise</h1>
-                <div className={`w-[90%] md:w-[65%] md:mt-0 mx-auto h-full flex flex-col justify-center items-center pb-10 md:pb-0 ${currentSection === 'Expertise' ? 'md:animate-bottom-top-fade-in border-l-2 border-accent' : 'md:opacity-0'}`}>
+                <h1 className={`hidden md:block absolute text-accent opacity-20 text-4xl md:text-8xl font-black -rotate-90 left-0 -translate-x-1/3 md:translate-x-0 duration-500 ${currentSection === 'Expertise' ? 'top-[42%] md:top-[45%] -translate-y-1/2' : 'md:top-[150%]'}`}>Expertise</h1>
+                <div className={`w-[90%] md:w-[65%] md:mt-0 mx-auto h-full flex flex-col justify-center items-center pb-10 md:pb-0 border-l-2 border-accent ${currentSection === 'Expertise' ? 'md:animate-bottom-top-fade-in' : 'md:opacity-0'}`}>
                     <h2 className='my-10 text-accent font-bold text-3xl block md:hidden'>Expertise</h2>
                     <div className='relative text-left w-full pl-6'>
                         <span className='absolute h-6 w-6 bg-accent -left-3 rotate-45 top-2'></span>
@@ -28,6 +28,11 @@ const Expertise = ({ currentSection }) => {
                         <span className='absolute h-6 w-6 bg-accent -left-3 rotate-45 top-2'></span>
                         <h3 className='font-bold text-3xl text-accent border-b-2 w-fit'>Tools:</h3>
                         <p className='mt-3 text-neutral text-xl'>Git, Github, Firebase, Netlify, Heroku, Dev Tools, React Dev Tools, Figma.</p>
+                    </div>
+                    <div className='relative text-left w-full pl-6 mt-10'>
+                        <span className='absolute h-6 w-6 bg-accent -left-3 rotate-45 top-2'></span>
+                        <h3 className='font-bold text-3xl text-accent border-b-2 w-fit'>Languages:</h3>
+                        <p className='mt-3 text-neutral text-xl'>English, Bengali, Hindi.</p>
                     </div>
                 </div>
             </div>

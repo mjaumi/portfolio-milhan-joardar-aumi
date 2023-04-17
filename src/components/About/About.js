aec846bc78fd96f6a08fc376b6fe1fd59d943ebb
mjaumi <mjaumi2864@gmail.com>
2023-04-17 22:05:31 +0600

    about me rewrite done

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -15,10 +15,10 @@ const About = ({ setCurrentSection }) => {
             <InView as='div' threshold={1.0} onChange={inView => inView && setCurrentSection('About')}>
                 <animated.div ref={ref} style={animProps} className='py-20 flex flex-col-reverse xl:flex-row items-center w-[95%] md:w-4/5 mx-auto'>
                     <div className='mt-10 xl:mt-0 xl:text-left xl:w-2/3'>
-                        <p className='text-neutral text-sm md:text-base'>Greetings, I'm <span className='text-accent font-semibold'>Milhan Joardar Aumi</span>, a proactive and enthusiastic <span className='text-accent font-semibold'>Front-end Developer</span>. I am currently pursuing my undergraduate degree on <span className='text-accent font-semibold'>Computer Science & Engineering</span> from <span className='text-accent font-semibold'> Ahsanullah University of Science & Technology</span>. I have accomplished an online course named <span className='text-accent font-semibold'>Complete Web Development Course With Jhankar Mahbub</span> from <span className='text-accent font-semibold'>Programming Hero</span> on May 31, 2022. Throughout the course I have learned many things as HTML5, CSS3, JavaScript, ECMAScript, REST API, Chrome Dev Tools, React.js, React Router, MongoDB, Node.js, Express.js, React Query etc. I have completed 12 assignments through out the course as well. My goal is to become a Professional Front-end Developer in a renowned company and enhance my knowledge with learning new things.</p>
+                        <p className='text-neutral text-sm md:text-base'>Greetings, I'm <span className='text-accent font-semibold'>Milhan Joardar Aumi</span>, a proactive and enthusiastic <span className='text-accent font-semibold'>MERN Stack Developer</span> from Dhaka, Bangladesh. I have completed my graduation on <span className='text-accent font-semibold'>Computer Science & Engineering</span> from <span className='text-accent font-semibold'> Ahsanullah University of Science & Technology</span>. My passion for web development started from the very first year of my university life. I have completed two online courses based on web development and also completed an internship on it. My goal is to become a Professional MERN Stack Developer in a renowned company and enhance my knowledge with learning new things.</p>
                     </div>
-                    <div className='xl:ml-20 overflow-hidden rounded-lg shadow-xl xl:w-1/3'>
-                        <img className='w-full' src={me} alt='me' />
+                    <div className='xl:ml-20 flex justify-center shadow-xl xl:w-1/3'>
+                        <img className='w-1/2 xl:w-full rounded-lg' src={me} alt='me' />
                     </div>
                 </animated.div>
             </InView>

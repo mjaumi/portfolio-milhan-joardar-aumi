fc25f845febe477f097142c3a387abc1822afe14
mjaumi <mjaumi2864@gmail.com>
2024-07-12 22:03:31 +0600

    adding index into experties

--- src/components/Expertise/Expertise.js
+++ src/components/Expertise/Expertise.js
@@ -25,7 +25,7 @@ const Expertise = ({ setCurrentSection }) => {
             <InView as='div' threshold={0.8} onChange={inView => inView && setCurrentSection('Expertise')}>
                 <animated.div ref={ref} style={animProps} className='py-20 grid grid-cols-3 md:grid-cols-6 xl:grid-cols-8 2xl:grid-cols-10 gap-5 w-[95%] xl:w-4/5 mx-auto'>
                     {
-                        skillSet?.map(skill => <ExpertiseItem
+                        skillSet?.map((skill) => <ExpertiseItem
                             key={skill.id}
                             skill={skill}
                         />)
